4ce5bc7a1060c632dcb75b3632037a15cbbf5595
GitHub Action <action@github.com>
2021-04-24 21:48:47 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/potentii/blenderideaplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.potentii.blenderideaplugin.services
+
+import com.github.potentii.blenderideaplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
